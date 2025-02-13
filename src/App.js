ef4d294f074573a113b7d68d62437512a159ec93
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-02-14 01:48:32 +0200

    Fixing Footer Position

--- src/App.js
+++ src/App.js
@@ -8,48 +8,55 @@ import NavBar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Job_Details from "./Pages/Job_Details";
 import JobList from "./Pages/JobList";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="">
+    <div className="" style={{}}>
+      {/* <Container style={{ minHeight: "100vh", backgroundColor: "yellow" }}> */}
       <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/Freelancia-Front-End" component={Home} exact />
-          <Route
-            path="/Freelancia-Front-End/login"
-            component={LoginForm}
-            exact
-          />
-          <Route path="/login" component={LoginForm} exact />
-          <Route
-            path="/Freelancia-Front-End/register"
-            component={RegisterForm}
-            exact
-          />
-          <Route path="/register" component={RegisterForm} exact />
-          <Route
-            path="/Freelancia-Front-End/job_list"
-            component={JobList}
-            exact
-          />
-          <Route path="/job_list" component={JobList} exact />
-          <Route
-            path="/job_details/:project_id"
-            component={Job_Details}
-            exact
-          />
-          <Route
-            path="/Freelancia-Front-End/job_details/:project_id"
-            component={Job_Details}
-            exact
-          />
+        <div className="GridCustom" style={{}}>
+          <NavBar />
 
-          <Route path="*" component={Home} />
-        </Switch>
-        <Footer />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/Freelancia-Front-End" component={Home} exact />
+            <Route
+              path="/Freelancia-Front-End/login"
+              component={LoginForm}
+              exact
+            />
+            <Route path="/login" component={LoginForm} exact />
+            <Route
+              path="/Freelancia-Front-End/register"
+              component={RegisterForm}
+              exact
+            />
+            <Route path="/register" component={RegisterForm} exact />
+            <Route
+              path="/Freelancia-Front-End/job_list"
+              component={JobList}
+              exact
+            />
+            <Route path="/job_list" component={JobList} exact />
+            <Route
+              path="/job_details/:project_id"
+              component={Job_Details}
+              exact
+            />
+            <Route
+              path="/Freelancia-Front-End/job_details/:project_id"
+              component={Job_Details}
+              exact
+            />
+
+            <Route path="*" component={Home} />
+          </Switch>
+
+          <Footer />
+        </div>
       </BrowserRouter>
+      {/* </Container> */}
     </div>
   );
 }
