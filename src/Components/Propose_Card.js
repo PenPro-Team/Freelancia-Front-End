0d174b98d0a3b8bad0c8c71b6d5c565b649d4fc5
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-02-16 08:08:28 +0200

    Handling Proposal Text

--- src/Components/Propose_Card.js
+++ src/Components/Propose_Card.js
@@ -7,30 +7,47 @@ import {
 } from "react-bootstrap";
 import { useState } from "react";
 import Propose_Msg from "./Propose_Msg";
+import { getFromLocalStorage } from "../network/local/LocalStorage";
 
 function Propose_Card(props) {
+  const [isUpdate, setIsUpdae] = useState(false);
+  const auth = getFromLocalStorage("auth");
+  const user = auth.user;
+  const isAuth = auth.isAuthenticated;
+  const handlecb = (value = false) => {
+    setIsUpdae(value);
+  };
   return (
-    <div>
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <AccordionHeader>Make a Proposal</AccordionHeader>
-          <Accordion.Body>
-            <Card>
-              <Card.Body>
-                <h5 className="card-title">Proposal Title</h5>
-                <div>
-                  <Propose_Msg
-                    user={props.user}
-                    project_id={props.project_id}
-                    disabled={props.disabled}
-                  />
-                </div>
-              </Card.Body>
-            </Card>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </div>
+    <>
+      {auth && isAuth && user.role == "freelancer" && (
+        <div>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <AccordionHeader>Handle your Proposal</AccordionHeader>
+              <Accordion.Body>
+                <Card>
+                  <Card.Body>
+                    {isUpdate ? (
+                      <h5 className="card-title">Update Your Proposal</h5>
+                    ) : (
+                      <h5 className="card-title">Create A New Proposal</h5>
+                    )}
+                    <div>
+                      <Propose_Msg
+                        user={props.user}
+                        project_id={props.project_id}
+                        disabled={props.disabled}
+                        cb={handlecb}
+                      />
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
+      )}
+    </>
   );
 }
 
