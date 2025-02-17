26253616ebfac0c1c599865260251f1afd269c78
mustafajuba98 <justjoba3@gmail.com>
2025-02-17 04:03:47 +0200

    handling some erros and modifing some pages (createjob , input field , register) , also installed react icons ...

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, InputGroup } from "react-bootstrap";
+import { Eye, EyeSlash } from "react-bootstrap-icons";
 
 const InputField = ({
   label,
@@ -13,27 +14,64 @@ const InputField = ({
   as,
   ...props
 }) => {
+  const isPasswordField = type === "password";
+  const [showPassword, setShowPassword] = useState(false);
+  const inputType = isPasswordField ? (showPassword ? "text" : "password") : type;
+
   return (
     <Form.Group controlId={name} className={className}>
-      <Form.Label className="mb-1" style={{ fontSize: "0.875rem", fontWeight: "bold" }}>
+      <Form.Label
+        className="mb-1"
+        style={{ fontSize: "0.875rem", fontWeight: "bold" }}
+      >
         {label}
       </Form.Label>
-      <Form.Control
-        as={as}
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        isInvalid={isInvalid}
-        style={{ height: "38px" }}
-        {...props}
-      />
-      <Form.Control.Feedback
-        type="invalid"
-        style={{ fontSize: "0.75rem", color: "#dc3545" }} // تقليل حجم النص وتغيير اللون
-      >
-        {feedback}
-      </Form.Control.Feedback>
+      {isPasswordField ? (
+        <InputGroup hasValidation>
+          <Form.Control
+            as={as}
+            type={inputType}
+            name={name}
+            value={value}
+            onChange={onChange}
+            isInvalid={isInvalid}
+            style={{ height: "38px" }}
+            {...props}
+          />
+          <InputGroup.Text
+            className="d-flex align-items-center"
+            style={{ cursor: "pointer" }}
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <EyeSlash size={20} /> : <Eye size={20} />}
+          </InputGroup.Text>
+          <Form.Control.Feedback
+            type="invalid"
+            style={{ fontSize: "0.75rem", color: "#dc3545" }}
+          >
+            {feedback}
+          </Form.Control.Feedback>
+        </InputGroup>
+      ) : (
+        <>
+          <Form.Control
+            as={as}
+            type={inputType}
+            name={name}
+            value={value}
+            onChange={onChange}
+            isInvalid={isInvalid}
+            style={{ height: "38px" }}
+            {...props}
+          />
+          <Form.Control.Feedback
+            type="invalid"
+            style={{ fontSize: "0.75rem", color: "#dc3545" }}
+          >
+            {feedback}
+          </Form.Control.Feedback>
+        </>
+      )}
     </Form.Group>
   );
 };
