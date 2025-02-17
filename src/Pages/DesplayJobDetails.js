505ff3d81743c856508a6a8a8d5a7f6209939a5b
Mo2024-cloud <mrfreelancer.org@gmail.com>
2025-02-17 00:18:58 -0800

    handleError

--- src/Pages/DesplayJobDetails.js
+++ src/Pages/DesplayJobDetails.js
@@ -1,11 +1,33 @@
 
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { getFromLocalStorage } from "../network/local/LocalStorage";
 import ClientJobList from "./ClientJobList";
+import { useEffect } from "react";
 
 function DesplayJobDetails() {
       const auth = getFromLocalStorage("auth");
-      const user = auth ? auth.user : null;
-      const isAuth = auth ? auth.isAuthenticated : null;
+    //   const user = auth ? auth.user : null;
+    //   const isAuth = auth ? auth.isAuthenticated : null;
+    const History = useHistory()
+    useEffect(() => {
+        if (!auth){
+            History.push("/Freelancia-Front-End")
+        }
+        else{
+            if(!auth.isAuthenticated){
+                History.push("/Freelancia-Front-End")
+            }else{
+                if(!auth.user){
+                    History.push("/Freelancia-Front-End")
+                }else{
+                    if(auth.user.role !== "client"){
+                        History.push("/Freelancia-Front-End")
+                    }
+                }
+            }
+        }
+        
+    },[])
   return (
     <div className="m-3">
         <p
@@ -17,8 +39,13 @@ function DesplayJobDetails() {
           }}
         >
           Job Lists
-        </p>  
-        <ClientJobList userId={user.id} />
+        </p> 
+        {auth && auth.isAuthenticated && auth.user && auth.user.role === "client" ? (
+            <ClientJobList userId={auth.user.id} />
+          ) : (
+            History.push("/unauthrizedpage")
+          )} 
+        
     </div>
   );
 }
