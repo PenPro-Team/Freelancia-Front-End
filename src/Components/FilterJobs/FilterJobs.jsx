3386c370684ed34e5c532bb54f05f0961081b156
jackiee211 <nggaman111@gmail.com>
2025-02-18 05:42:44 +0200

    added Recent Jobs filter

--- src/Components/FilterJobs/FilterJobs.jsx
+++ src/Components/FilterJobs/FilterJobs.jsx
@@ -1,36 +1,25 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-export default function FilterJobs() {
+export default function FilterJobs({ onSortRecent, isSortedByRecent }) {
   return (
     <>
       <ul className='list-unstyled list-inline'>
+        {/* <li className='list-inline-item'>
+          <Button variant="primary" style={{ marginBottom: "15px" }}>
+            Top Clients
+          </Button>
+        </li> */}
         <li className='list-inline-item'>
-          <Link
-            className='btn btn-primary text-light text-decoration-none h6 p-1 rounded-3'
-            to="#"
-            aria-label="Recommended Jobs"
-          >
+          <Button variant="primary" onClick={onSortRecent} style={{ marginBottom: "15px" }}>
+            {isSortedByRecent ? "Reset Order" : "Sort by Recent Jobs"}
+          </Button>
+        </li>
+        <li className='list-inline-item'>
+          <Button variant="primary" style={{ marginBottom: "15px" }}>
             Recommended Jobs
-          </Link>
-        </li>
-        <li className='list-inline-item'>
-          <Link
-            className='btn btn-primary text-light text-decoration-none h6 p-1 rounded-3'
-            to="#"
-            aria-label="Recent Jobs"
-          >
-            Recent Jobs
-          </Link>
-        </li>
-        <li className='list-inline-item'>
-          <Link
-            className='btn btn-primary text-light text-decoration-none h6 p-1 rounded-3'
-            to="#"
-            aria-label="Top Rated Clients"
-          >
-            Top Rated Clients
-          </Link>
+          </Button>
         </li>
       </ul>
       <hr />
