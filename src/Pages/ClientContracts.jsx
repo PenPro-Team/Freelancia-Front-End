e0e43adb645e0ff6c0252340822955fb4ac06be2
AhmedHamdy85 <ahme.hamdy85@gmail.com>
2025-04-05 22:03:59 +0200

    handel contract states and attatchments

--- src/Pages/ClientContracts.jsx
+++ src/Pages/ClientContracts.jsx
@@ -5,38 +5,41 @@ import { useParams } from 'react-router-dom';
 import ContractCard from '../Components/ContractCard';
 import HeaderColoredText from '../Components/HeaderColoredText';
 import { getFromLocalStorage } from '../network/local/LocalStorage';
+
 function ClientContracts() {
-    const prams=useParams();
-  const [contracts, setContracts] = useState([]);
-const current_user = getFromLocalStorage("auth");
-  useEffect(() => {
-    AxiosContractsInstance.get(`user/contracts/${current_user.user.user_id}`,
-      {
-        headers: {
-          'Authorization': `Bearer ${current_user.user.access}`
-        }
-      }
-    )
-      .then((response) => {
-        setContracts(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [prams.user_id]);
-  return (
-    <Container className="py-4">
-        <HeaderColoredText text="Your Contracts" />
-      
-      {contracts.length > 0 ? (
-        contracts.map(contract => (
-          <ContractCard key={contract.id} contract={contract} />
-        ))
-      ) : (
-        <p>No contracts found.</p>
-      )}
-    </Container>
-  );
+    const prams = useParams();
+    const [contracts, setContracts] = useState([]);
+    const current_user = getFromLocalStorage("auth");
+
+    useEffect(() => {
+        AxiosContractsInstance.get(`user/contracts/${current_user.user.user_id}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${current_user.user.access}`
+                }
+            }
+        )
+            .then((response) => {
+                setContracts(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [current_user.user.user_id]); // Changed from prams.user_id to current_user.user.user_id
+
+    return (
+        <Container className="py-4">
+            <HeaderColoredText text="Your Contracts" />
+
+            {contracts.length > 0 ? (
+                contracts.map(contract => (
+                    <ContractCard key={contract.id} contract={contract} />
+                ))
+            ) : (
+                <p>No contracts found.</p>
+            )}
+        </Container>
+    );
 }
 
 export default ClientContracts;
